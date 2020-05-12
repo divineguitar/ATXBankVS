da53f620df86bde20a8e4fb15a80c084d6bce815
divineguitar <31075906+divineguitar@users.noreply.github.com>
2020-05-12 13:06:43 -0500

    Add project files.

---
+++ ATXBank/Data/ATXBankContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ATXBank.Models;
+
+namespace ATXBank.Data
+{
+    public class ATXBankContext : DbContext
+    {
+        public ATXBankContext (DbContextOptions<ATXBankContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+
+        }
+
+        public DbSet<ATXBank.Models.Employee> Employee { get; set; }
+    }
+}
